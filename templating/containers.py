65582f26b6706a0b6f4000464ca1ef47bc3a0d16
NumberJamie <jamievanessen@gmail.com>
2024-01-04 15:20:08 +0100

    [New] Attr functions, Tags

--- templating/containers.py
+++ templating/containers.py
@@ -3,6 +3,26 @@ from typing import Union
 from .tag import BaseElement
 
 
+class Address(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'address'
+
+
+class Article(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'article'
+
+
+class Body(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'base'
+
+    # TODO: add body attributes: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/body
+
+
 class Div(BaseElement):
     def __init__(self, *content: Union[str, BaseElement]):
         super().__init__(*content)
--- templating/tag.py
+++ templating/tag.py
@@ -21,8 +21,8 @@ class BaseElement:
         return self
 
     def _get_meta(self) -> str:
-        ids = f' id=\'{' '.join(self._id_list)}\'' if self._id_list else ''
-        classes = f' class=\'{' '.join(self._class_list)}\'' if self._class_list else ''
+        ids = f' id=\'{" ".join(self._id_list)}\'' if self._id_list else ''
+        classes = f' class=\'{" ".join(self._class_list)}\'' if self._class_list else ''
         return f'{ids}{classes}'
 
     def _get_content(self) -> str:
@@ -35,6 +35,20 @@ class BaseElement:
                 continue
             content_str += f'\n\t{str(content)}'
         return content_str
+
+    @staticmethod
+    def _option_attr(name: str, target: str, allowed: list[str]):
+        if target not in allowed:
+            raise ValueError(f'Used invalid target: {target} allowed values are: {allowed}.')
+        return f' {name}=\'{target}\''
+
+    @staticmethod
+    def _bool_attr(bools: list[str], allowed: list[str]):
+        for i in bools:
+            if i in allowed:
+                continue
+            raise ValueError(f'Used invalid boolean: {i} valid booleans are: {allowed}.')
+        return f' {" ".join(bools)}' if bools else ''
 
     def _additional(self):
         return ''
@@ -80,11 +94,136 @@ class A(BaseElement):
         return self
 
     def target(self, target: str):
-        valid_target = ['_self', '_blank', '_parent', '_top']
-        if target not in valid_target:
-            raise ValueError(f'Used invalid target: {target} valid targets are: {valid_target}.')
-        self._target = f' target=\'{target}\''
+        self._target = self._option_attr('target', target, ['_self', '_blank', '_parent', '_top'])
         return self
+
+
+class Abbr(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'abbr'
+        self._title: str = ''
+
+    def _additional(self):
+        return f'{self._title}'
+
+    def title(self, title: str):
+        self._title = f' title=\'{title}\''
+        return self
+
+
+class Aside(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'aside'
+
+
+class Audio(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._bool_list: list = []
+        self._bools: str = ''
+        self._src: str = ''
+        self._preload: str = ''
+        self._controls_list: str = ''
+        self._tag = 'audio'
+
+    def _additional(self):
+        return f'{self._bools}{self._src}{self._preload}{self._controls_list}'
+
+    def src(self, src: str):
+        self._src = f' src=\'{src}\''
+        return self
+
+    def bools(self, *bools):
+        self._bool_list.extend(bools)
+        self._bools = self._bool_attr(self._bool_list,
+                                      ['autoplay', 'controls', 'disableremoteplayback', 'loop', 'muted'])
+        return self
+
+    def preload(self, target: str):
+        self._preload = self._option_attr('preload', target, ['none', 'metadata', 'auto'])
+        return self
+
+    def controls_list(self, target: str):
+        self._controls_list = self._option_attr('controlslist', target,
+                                                ['nodownload', 'nofullscreen', 'noremoteplayback'])
+        return self
+
+
+class B(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'b'
+
+
+class Base(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'base'
+        self._href: str = ''
+        self._target: str = ''
+
+    def _additional(self):
+        return f'{self._target}{self._href}'
+
+    def target(self, target: str):
+        self._target = self._option_attr('target', target, ['_self', '_blank', '_parent', '_top'])
+        return self
+
+    def href(self, href: str):
+        if href.startswith('data:') or href.startswith('javascript:'):
+            raise ValueError(f'Base tag href cannot start with data: or javascript:')
+        self._href = f' href=\'{href}\''
+        return self
+
+
+class Bdi(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'bdi'
+
+
+class Bdo(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._tag = 'bdo'
+
+
+class BlockQuote(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._cite: str = ''
+        self._tag = 'blockquote'
+
+    def _additional(self):
+        return f'{self._cite}'
+
+    def cite(self, cite: str):
+        self._cite = f' cite=\'{cite}\''
+        return self
+
+
+# TODO: add <br> tag, maybe just use a variable?!
+
+
+class Button(BaseElement):
+    def __init__(self, *content: Union[str, BaseElement]):
+        super().__init__(*content)
+        self._bool_list: list = []
+        self._bools: str = ''
+        self._tag = 'button'
+
+    def _additional(self):
+        return f'{self._bools}'
+
+    def bools(self, *bools):
+        self._bool_list.extend(bools)
+        self._bools = self._bool_attr(self._bool_list,
+                                      ['autofocus', 'disabled', 'formnovalidate'])
+        return self
+
+    # TODO: add non-boolean attributes
 
 
 class Small(BaseElement):
@@ -97,5 +236,3 @@ class Span(BaseElement):
     def __init__(self, *content: Union[str, BaseElement]):
         super().__init__(*content)
         self._tag = 'span'
-
-
